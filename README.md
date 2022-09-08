5b6cd9c292a2c99a36d1926983a2dd4cf961a549
alex_black <alexblacknn@yandex.ru>
2022-09-08 21:34:27 +0300

    change readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # praktikum_new_diplom
+
+пока запускаемся так:
+
+ФРОНТ 
+
+cd frontend
+npm run start 
+
+Бэк 
+
+python manage.py runserver
--- frontend/src/configs/navigation.js
+++ frontend/src/configs/navigation.js
@@ -1,23 +1,23 @@
 export default [
   {
     title: 'Рецепты',
-    href: '/recipes',
+    href: '/recipes:8000',
     auth: false
   },{
     title: 'Мои подписки',
-    href: '/subscriptions',
+    href: '/subscriptions:8000',
     auth: true
   },{
     title: 'Создать рецепт',
-    href: '/recipes/create',
+    href: '/recipes/create:8000',
     auth: true
   },{
     title: 'Избранное',
-    href: '/favorites',
+    href: '/favorites:8000',
     auth: true
   }, {
     title: 'Список покупок',
-    href: '/cart',
+    href: '/cart:8000',
     auth: true
   }
 ]
