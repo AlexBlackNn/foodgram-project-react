7fbc12483a5cc1d77a4c26bfb1a11e5cc2ad2c56
alex_black <alexblacknn@yandex.ru>
2022-09-23 18:48:45 +0300

    ci

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py loaddata init_database.json
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000 --timeout 90
