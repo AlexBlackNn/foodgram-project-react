a6ceff4ca97aa08ea27a0edb8d945531f43d8c0e
alex black <alexblacknn@yandex.ru>
2022-09-23 14:05:01 +0300

    isort

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,6 @@ from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
                             ShoppingList, Tag)
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
 from users.models import User
 from users.serializers import UserSerializer
 
