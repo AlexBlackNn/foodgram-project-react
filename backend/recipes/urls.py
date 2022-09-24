f4ada32a7fb22c5f0d58467dfe5332f475f81ef5
alex_black <alexblacknn@yandex.ru>
2022-09-24 19:54:09 +0300

    fix pep8

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (DownloadShoppingCart, IngredientViewSet,
-                    RecipeViewSet, TagView)
+from .views import (DownloadShoppingCart, IngredientViewSet, RecipeViewSet,
+                    TagView)
 
 router = DefaultRouter()
 router.register('tags', TagView, basename='tags')
