f46093959440cbc1c39faa31b7bb5451df52634c
alex_black <alexblacknn@yandex.ru>
2022-09-26 15:15:59 +0300

    pep8

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,11 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    IngredientViewSet,
-    RecipeViewSet,
-    TagView
-)
+from .views import IngredientViewSet, RecipeViewSet, TagView
 
 router = DefaultRouter()
 router.register('tags', TagView, basename='tags')
